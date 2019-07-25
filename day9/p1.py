2e4f77245ec776de137b8cb501aabe3bd5f02ad5
JeevaSasikumar <46344027+JeevaSasikumar@users.noreply.github.com>
2019-07-25 11:47:44 +0530

    Create p1.py

---
+++ day9/p1.py
@@ -0,0 +1,34 @@
+a=input()
+if(len(a)>2):
+    b=a[len(a)-3:]
+    if(b=="ing"):
+        a+="ly"
+        print(a)
+    else:
+        a+="ing"
+        print(a)a=input()
+if(len(a)>2):
+    b=a[len(a)-3:]
+    if(b=="ing"):
+        a+="ly"
+        print(a)
+    else:
+        a+="ing"
+        print(a)
+else:
+    print(a)
+        
+else:
+    print(a)a=input()
+if(len(a)>2):
+    b=a[len(a)-3:]
+    if(b=="ing"):
+        a+="ly"
+        print(a)
+    else:
+        a+="ing"
+        print(a)
+else:
+    print(a)
+        
+        
